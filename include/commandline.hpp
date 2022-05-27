951916f51111c8f51bcbc4381cb0cbfe6e335444
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2022-05-26 18:12:10 -0600

    make cmdl easier to add to and modify

--- include/commandline.hpp
+++ include/commandline.hpp
@@ -3,12 +3,29 @@
 #ifndef _COMMANDLINE_HPP_
 #define _COMMANDLINE_HPP_
 
-std::map<std::string, std::string> arguments {{"threads", "Sets the number of threads"}, {"wordlist", "Sets the wordlist"}, {"parallel", "Sets the number of parallel wordles"}, {"hardmode", "Sets the hardmode"}, {"searchmode", "Sets the searchmode"}};
+namespace cmdl {
+  class NAMES {
+    public:
+      const inline static std::string THREAD_ARG = "threads";
+      const inline static std::string WORDLIST_ARG = "wordlist";
+      const inline static std::string PARALLEL_ARG = "parallel";
+      const inline static std::string HARDMODE_ARG = "hardmode";
+      const inline static std::string SEARCHMODE_ARG = "searchmode";
+      const inline static std::string FINDBEST_CMD = "findbest";
+      const inline static std::string LIST_CMD = "list";
+      const inline static std::string RATE_CMD = "rate";
+      const inline static std::string FILTER_CMD = "filter";
+  };
 
-std::map<std::string, std::string> commands {{"findbest", "Finds best words based on inputted guesses. Seperate guesses with spaces"}, {"list", "Lists remaining possible words. Seperate guesses with spaces"}, {"rate", "Rates the inputted words. Seperate words with spaces"}};
+  const std::map<std::string, std::string> arguments {{NAMES::THREAD_ARG, "Sets the number of threads"}, {NAMES::WORDLIST_ARG, "Sets the wordlist"}, {NAMES::PARALLEL_ARG, "Sets the number of parallel wordles"}, {NAMES::HARDMODE_ARG, "Sets the hardmode"}, {NAMES::SEARCHMODE_ARG, "Sets the searchmode"}};
 
-std::vector<std::string> examples {"./wordl -threads=4 -wordlist=nytimes2 -parallel=1 -hardmode=n -searchmode=2 findbest trace 00000 jazzy 00101"};
+  const std::map<std::string, std::string> commands {{NAMES::FINDBEST_CMD, "Finds best words based on inputted guesses. Seperate guesses with spaces"}, {NAMES::LIST_CMD, "Lists remaining possible words. Seperate guesses with spaces"}, {NAMES::RATE_CMD, "Rates the inputted words. Seperate words with spaces"}, {NAMES::FILTER_CMD, "Filters out words based on inputted words and ratings"}};
 
-std::vector<std::string> notes {"You do not need to use all of the arguments, and they do not need to be in the example's specific order", "You can only use one command"};
+  const std::vector<std::string> examples {"./wordl -" + NAMES::THREAD_ARG + "=4 -" + NAMES::WORDLIST_ARG + "=nytimes2 -" + NAMES::PARALLEL_ARG + "=1 -" + NAMES::HARDMODE_ARG + "=n -" + NAMES::SEARCHMODE_ARG + "=2 " + NAMES::FINDBEST_CMD + " trace 00000 jazzy 00101"};
+
+  const std::vector<std::string> notes {"You do not need to use all of the arguments, and they do not need to be in the example's specific order", "You can only use one command"};
+
+  const std::vector<std::string> moreLoops {NAMES::FINDBEST_CMD, NAMES::LIST_CMD};
+}
 
 #endif

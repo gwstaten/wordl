f83049224132dfc6e23e69c7e81ee1c78f71065b
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2022-05-28 21:50:48 -0600

    cmdl -help wording changes

--- include/commandline.hpp
+++ include/commandline.hpp
@@ -19,9 +19,9 @@ namespace cmdl {
 
   const std::map<std::string, std::string> arguments {{NAMES::THREAD_ARG, "Sets the number of threads"}, {NAMES::WORDLIST_ARG, "Sets the wordlist"}, {NAMES::PARALLEL_ARG, "Sets the number of parallel wordles"}, {NAMES::HARDMODE_ARG, "Sets the hardmode"}, {NAMES::SEARCHMODE_ARG, "Sets the searchmode"}};
 
-  const std::map<std::string, std::string> commands {{NAMES::FINDBEST_CMD, "Finds best words based on inputted guesses. Seperate guesses with spaces"}, {NAMES::LIST_CMD, "Lists remaining possible words. Seperate guesses with spaces"}, {NAMES::RATE_CMD, "Rates the inputted words. Seperate words with spaces"}, {NAMES::FILTER_CMD, "Filters out words based on inputted words and ratings"}};
+  const std::map<std::string, std::string> commands {{NAMES::FINDBEST_CMD, "Finds best words based on inputted guesses and ratings. Seperate guesses with spaces"}, {NAMES::LIST_CMD, "Lists remaining possible words based on inputted guesses and ratings. Seperate guesses with spaces"}, {NAMES::RATE_CMD, "Rates the inputted words. Seperate words with spaces"}, {NAMES::FILTER_CMD, "Shows amount of remaining words based on inputted guesses and ratings. Seperate guesses and ratings with spaces"}};
 
-  const std::vector<std::string> examples {"./wordl -" + NAMES::THREAD_ARG + "=4 -" + NAMES::WORDLIST_ARG + "=nytimes2 -" + NAMES::PARALLEL_ARG + "=1 -" + NAMES::HARDMODE_ARG + "=n -" + NAMES::SEARCHMODE_ARG + "=2 " + NAMES::FINDBEST_CMD + " trace 00000 jazzy 00101"};
+  const std::vector<std::string> examples {"./wordl -" + NAMES::THREAD_ARG + "=4 -" + NAMES::WORDLIST_ARG + "=nytimes2 -" + NAMES::PARALLEL_ARG + "=1 -" + NAMES::HARDMODE_ARG + "=n -" + NAMES::SEARCHMODE_ARG + "=2 " + NAMES::FINDBEST_CMD + " trace 00000 jazzy 00001"};
 
   const std::vector<std::string> notes {"You do not need to use all of the arguments, and they do not need to be in the example's specific order", "You can only use one command"};
 

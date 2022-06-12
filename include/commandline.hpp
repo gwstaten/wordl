1ca5920152928f226ad230572f551fd34d99f335
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2022-06-11 20:58:17 -0600

    fix typos and more notes

--- include/commandline.hpp
+++ include/commandline.hpp
@@ -23,11 +23,11 @@ namespace cmdl {
 
   const std::map<std::string, std::string> arguments {{NAMES::THREAD_ARG, "Sets the number of threads"}, {NAMES::WORDLIST_ARG, "Sets the wordlist"}, {NAMES::HARDMODE_ARG, "Sets the hardmode"}, {NAMES::SEARCHMODE_ARG, "Sets the searchmode"}, {NAMES::PREFIX_ARG, "Sets prefix words. Only for " + NAMES::FINDBEST_CMD}, {NAMES::ULETTER_ARG, "Sets the number of unique letters"}, {NAMES::UFILTERBY_ARG, "Sets filter mode. Only for " + NAMES::UFILTER_CMD}};
 
-  const std::map<std::string, std::string> commands {{NAMES::FINDBEST_CMD, "Finds best words based on inputted guesses and ratings. Seperate guesses with spaces"}, {NAMES::LIST_CMD, "Lists remaining possible words based on inputted guesses and ratings. Seperate guesses with spaces"}, {NAMES::RATE_CMD, "Rates the inputted words. Seperate words with spaces"}, {NAMES::FILTER_CMD, "Shows amount of remaining words based on inputted guesses and ratings. Seperate guesses and ratings with spaces"}, {NAMES::UFILTER_CMD, "Filters the answer list based on the number of unique letters. Requires " + NAMES::ULETTER_ARG + " and " + NAMES::UFILTERBY_ARG + " arguments"}};
+  const std::map<std::string, std::string> commands {{NAMES::FINDBEST_CMD, "Finds best words based on inputted guesses and ratings. Separate guesses with spaces"}, {NAMES::LIST_CMD, "Lists remaining possible words based on inputted guesses and ratings. Separate guesses with spaces"}, {NAMES::RATE_CMD, "Rates the inputted words. Separate words with spaces"}, {NAMES::FILTER_CMD, "Shows amount of remaining words based on inputted guesses and ratings. Separate guesses and ratings with spaces"}, {NAMES::UFILTER_CMD, "Filters the answer list based on the number of unique letters. Requires " + NAMES::ULETTER_ARG + " and " + NAMES::UFILTERBY_ARG + " arguments"}};
 
   const std::vector<std::string> examples {"./wordl -" + NAMES::THREAD_ARG + "=4 -" + NAMES::WORDLIST_ARG + "=nytimes2 -" + NAMES::HARDMODE_ARG + "=n -" + NAMES::SEARCHMODE_ARG + "=2 " + NAMES::FINDBEST_CMD + " trace 00000 jazzy 00001"};
 
-  const std::vector<std::string> notes {"You do not need to use all of the arguments, and they do not need to be in the example's specific order", "You can only use one command"};
+  const std::vector<std::string> notes {"You do not need to use all of the arguments, and they do not need to be in the example's specific order", "You can only use one command", "For arguments, you can leave the value blank (E.g. -wordlist=) or not include the equal sign (E.g. -wordlist) to have it set to it's default value"};
 
   const std::vector<std::string> moreLoops {NAMES::FINDBEST_CMD, NAMES::LIST_CMD};
 

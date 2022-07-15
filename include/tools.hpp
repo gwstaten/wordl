0c525482082fb595ec0b2e62d8faf11d0d9a85a5
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2022-07-14 20:25:44 -0700

    fix header guard not matching file name

--- include/tools.hpp
+++ include/tools.hpp
@@ -3,11 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-#ifndef _TOOL_HPP_
-#define _TOOl_HPP_
+#ifndef _TOOLS_HPP_
+#define _TOOLS_HPP_
 
 #include "util.hpp"
 
 void permute(std::vector<std::string> wordSet, std::vector<std::string> validGuesses);
 void order(std::vector<std::string> wordSet, std::vector<std::string> valids);
+
 #endif

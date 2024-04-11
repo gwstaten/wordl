88097e030fa8f83dbae60e02f78bd80941e38591
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2024-04-11 14:21:09 -0600

    add missing license to top of files

--- src/rate.cpp
+++ src/rate.cpp
@@ -1,3 +1,8 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 #include "rate.hpp"
 
 void rateAll(std::vector<std::string> guess, std::vector<std::string> words, std::string wordlist, bool rateFileColorings)
